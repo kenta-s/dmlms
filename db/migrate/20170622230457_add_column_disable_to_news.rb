3c150b54b4d3c4ea1b8dda03beff5afbce176b33
kenta-s <knt01222@gmail.com>
2017-06-24 07:37:08 +0900

    Modify migration file

--- db/migrate/20170622230457_add_column_disable_to_news.rb
+++ db/migrate/20170622230457_add_column_disable_to_news.rb
@@ -1,5 +1,10 @@
 class AddColumnDisableToNews < ActiveRecord::Migration[5.1]
-  def change
-    add_column :news, :disable, :boolean, null: false, default: false
+  def up
+    add_column :news, :disable, :boolean, default: false
+    change_column :news, :disable, :boolean, default: false, null: false # A hack for SQLite3
+  end
+
+  def down
+    remove_column :news, :disable
   end
 end
